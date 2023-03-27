c45622ac1969d25e3f1370e6c5a6bc676a2cebed
youngmettro12 <ianomurigu12@gmail.com>
2023-03-27 13:48:40 +0000

    updated 0x05-python-exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+afe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
